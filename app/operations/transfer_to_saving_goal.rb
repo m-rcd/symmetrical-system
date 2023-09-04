bc9087fe0ff4c3fc6962c588c88d7a21b13cc35e
Marianne Rachid <marianne.rachid@gmail.com>
2023-09-04 17:02:32 +0100

    Add TransferToSavingGoal operation

---
+++ app/operations/transfer_to_saving_goal.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+class TransferToSavingGoal 
+  def initialize(account_uid:, amount:)
+    @account_uid = account_uid
+    @amount = amount
+  end
+
+  class << self 
+    def call(account_uid:, amount:)
+      new(account_uid:, amount:).call
+    end
+end
+
+  def call 
+    create_saving_goal
+    transfer_to_saving_goal
+  end
+
+  private 
+
+  attr_reader :account_uid, :amount
+
+  def create_saving_goal
+    @saving_goal ||= StarlingApi::SavingGoal.create(account_uid)
+  end
+
+  def transfer_to_saving_goal
+    StarlingApi::SavingGoal.transfer_money(account_uid, @saving_goal["savingsGoalUid"], amount)
+  end
+
+end
---
+++ spec/operations/transfer_to_saving_goal_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe TransferToSavingGoal do 
+  subject { described_class.call(account_uid:, amount:) }
+
+  let(:account_uid) { "abc-123" }
+
+  let(:amount) { 34 }
+  let(:saving_goal) do
+    { "savingsGoalUid" => "cde-456" }
+  end
+
+  before do 
+    expect(StarlingApi::SavingGoal).to receive(:create).with(account_uid).and_return(saving_goal)
+  end
+
+  it "transfers the money to a new saving goal" do 
+    expect(StarlingApi::SavingGoal).to receive(:transfer_money).with(account_uid, saving_goal["savingsGoalUid"], amount)
+    subject
+  end
+  
+end 
