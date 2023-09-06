f97dfc3e6070fb41c10ab9dbb8bcdc99f390c2ec
Marianne Rachid <marianne.rachid@gmail.com>
2023-09-06 12:53:00 +0100

    Add Acceptance test

---
+++ spec/acceptance_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Acceptance', type: :request do
+  before { WebMock.allow_net_connect! }
+
+  describe 'POST /round_up' do
+    let(:expected_response) do
+      { 'round_up_amount' => '1500 pence (£15.00)' }.to_json
+    end
+
+    before { post '/round_up' }
+
+    it 'returns correct response' do
+      expect(response).to have_http_status(:success)
+      expect(response.body).to eq(expected_response)
+    end
+  end
+
+  describe 'POST /transfer' do
+    before { post '/transfer' }
+
+    it 'returns correct response' do
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body)['round_up_amount']).to eq('1500 pence (£15.00)')
+    end
+  end
+end
